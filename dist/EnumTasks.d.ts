00cc4a51e07463f267c2c4d906e10e681ab29b6b
John Hiesey <john@hiesey.com>
2021-09-29 12:13:28 -0700

    Built version

---
+++ dist/EnumTasks.d.ts
@@ -0,0 +1,12 @@
+import { Knex } from 'knex';
+import { Config, Enum } from './Typings';
+export declare function getAllEnums(db: Knex, config: Config): Promise<Enum[]>;
+/**
+ * Generates an enum name, removing invalid characters.
+ *
+ * @export
+ * @param {string} name The name of the enum.
+ * @param {Config} config The configuration to use.
+ * @returns
+ */
+export declare function generateEnumName(name: string, config: Config): string;
