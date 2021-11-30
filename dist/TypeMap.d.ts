60b228bdd79ec727b6553066d945e1b3d17b3424
mikola <mikolalysenko@gmail.com>
2021-11-30 16:04:49 -0500

    check build files into version control

---
+++ dist/TypeMap.d.ts
@@ -0,0 +1,18 @@
+declare const _default: {
+    global: {
+        string: string[];
+        'string[]': string[];
+        number: string[];
+        Date: string[];
+        boolean: string[];
+        Object: string[];
+        Buffer: string[];
+    };
+    postgres: {
+        string: string[];
+    };
+    mssql: {
+        string: string[];
+    };
+};
+export default _default;
